7c6b242ba0a9add28a47083dab6c7fa489a4f2a5
darrentran <darrenman456@outlook.com>
2019-01-20 01:20:14 -0700

    login and register does not actually work

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -9,33 +6,10 @@ import { FlashMessagesService } from 'angular2-flash-messages';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username: String;
-  password: String;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private flashMessage: FlashMessagesService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onLoginSubmit() {
-    const user = {
-      username: this.username,
-      password: this.password
-    }
-
-    this.authService.authenticateUser(user).subscribe(data => {
-        if(data.success) {
-          this.authService.storeUserData(data.token, data.user);
-          this.flashMessage.show('You are now logged in', {cssClass: 'alert-success', timeout: 5000});
-          this.router.navigate(['dashboard']);
-        } else {
-          this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 5000});
-          this.router.navigate(['login']);
-        }
-    });
   }
 
 }
